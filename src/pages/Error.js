e6a98abec4b184bbe4dd866843c788d265cae583
Md. Sakhawat Hossain <biplobbms@gmail.com>
2022-11-03 12:33:42 +0600

    React Routing Done

---
+++ src/pages/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+	return (
+		<div className='App'>
+			<h3>Route is not found.</h3>
+		</div>
+	);
+};
+
+export default Error;
---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './../pages/Home';
+import Error from './../pages/Error';
+import Navbar from './../pages/layouts/Navbar';
+
+const Index = () => {
+	return (
+		<BrowserRouter>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='*' element={<Error />} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
+
+export default Index;
