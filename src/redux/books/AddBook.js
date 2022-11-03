5c174b8b94d31d21fa599868e846f237ffc131ee
Md. Sakhawat Hossain <biplobbms@gmail.com>
2022-11-03 16:54:44 +0600

    React Redux Toolkit Done

--- src/redux/books/AddBook.js
+++ src/redux/books/AddBook.js
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { addBook } from './BooksSlice';
+// for unique id
+import { v4 as uuidv4 } from 'uuid';
 
 const AddBook = () => {
+	const [title, setTitle] = useState('');
+	const [author, setAuthor] = useState('');
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	// const numberOfBooks = useSelector(
+	// 	(state) => state.booksReducer.books.length
+	// );
+	const handleSubmit = (e) => {
+		// Not Value changes by refreshing
+		e.preventDefault();
+		// const book = { id: numberOfBooks + 1, title, author };
+		const book = { id: uuidv4(), title, author };
+		dispatch(addBook(book));
+		navigate('/show-books', { replace: true });
+	};
 	return (
 		<div>
 			<h2>Add Book</h2>
+			<form onSubmit={handleSubmit}>
+				<div className='form-field'>
+					<label htmlFor='title'>Title: </label>
+					<input
+						type='text'
+						id='title'
+						name='title'
+						value={title}
+						onChange={(e) => {
+							setTitle(e.target.value);
+						}}
+						required
+					/>
+				</div>
+				<div className='form-field'>
+					<label htmlFor='author'>Author: </label>
+					<input
+						type='text'
+						id='author'
+						name='author'
+						value={author}
+						onChange={(e) => {
+							setAuthor(e.target.value);
+						}}
+						required
+					/>
+				</div>
+				<button type='submit'>Add Book</button>
+			</form>
 		</div>
 	);
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,15 +3,23 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './../pages/Home';
 import Error from './../pages/Error';
 import Navbar from './../pages/layouts/Navbar';
+import BooksView from './../redux/books/BooksView';
+import AddBook from './../redux/books/AddBook';
+import EditBook from './../redux/books/EditBook';
 
 const Index = () => {
 	return (
 		<BrowserRouter>
 			<Navbar />
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='*' element={<Error />} />
-			</Routes>
+			<main>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/show-books' element={<BooksView />} />
+					<Route path='/add-book' element={<AddBook />} />
+					<Route path='/edit-book' element={<EditBook />} />
+					<Route path='*' element={<Error />} />
+				</Routes>
+			</main>
 		</BrowserRouter>
 	);
 };
